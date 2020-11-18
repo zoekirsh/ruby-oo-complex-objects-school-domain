302381214eb480112a2cd3dbe48b33a02a193c89
Zoe Kirsh <zoekirsh@gmail.com>
2020-11-18 13:43:44 -0800

    commit tree 52af92d9a02279883c3a378794e0c46907a842b2

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,29 @@
-# code here!
+require 'pry'
+class School
+
+    attr_reader :name, :roster
+
+    def initialize (name)
+        @name = name
+        @roster = {}
+    end
+
+    def add_student(student_name, grade)
+        @roster[grade] = [] if !@roster.include?(grade)
+        @roster[grade] << student_name
+    end
+
+    def grade(grade)
+        @roster[grade]
+    end
+
+    def sort
+        sorted_roster = @roster.sort.to_h
+        sorted_roster.each do |key, value|
+            value = value.sort!
+        end
+        sorted_roster
+    end
+
+
+end
